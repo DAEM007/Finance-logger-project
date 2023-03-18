41a3db351838da2a917d105a85dddc70857e41ac
Emmanuel <dammylola299@gmail.com>
2023-03-18 22:38:33 +0100

    updated payment class

---
+++ src/classes/payment.ts
@@ -0,0 +1,20 @@
+import { HasFormatter } from "../interfaces/HasFormatter";
+
+// class payment
+export class Payment implements HasFormatter {
+    readonly recipient: string;
+    private details: string;
+    public amount: number;
+
+    constructor(r: string, d: string, a: number) {
+        this.recipient = r;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.recipient} is owed ${this.amount} for ${this.details}`;
+    }
+
+}
+
