bc783090c00fbd03ff59e7386ffd73bd4ea318dc
Emmanuel <dammylola299@gmail.com>
2023-03-20 11:03:56 +0100

    updated the format method of the payment class

--- src/classes/payment.ts
+++ src/classes/payment.ts
@@ -13,7 +13,7 @@ export class Payment implements HasFormatter {
     }
 
     format() {
-        return `${this.recipient} is owed ${this.amount} for ${this.details}`;
+        return `${this.recipient} is owed $${this.amount} for ${this.details}`;
     }
 
 }
